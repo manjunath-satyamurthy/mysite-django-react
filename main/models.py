97d7fda80f30936d50974897e811fb88f15dba77
Manjunath Satyamurthy <pass2rahul@gmail.com>
2018-03-23 11:04:54 +0100

    import os

--- main/models.py
+++ main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import AbstractUser
+import os
 
 class RootUser(AbstractUser):
     profile_photo = models.ImageField(upload_to="profile")
